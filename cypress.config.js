1ccfffcb1eec78a4724185cab60ea2ccd4aa9fd7
BichNTN <bichntn.lqa@gmail.com>
2023-04-21 14:26:48 +0700

    report

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,42 @@
 const { defineConfig } = require("cypress");
 const fs = require('fs')
 const xlsx = require('xlsx');
+const cucumber = require('cypress-cucumber-preprocessor').default;
 
 module.exports = defineConfig({
   projectId: "99m3w8",
   chromeWebSecurity: false,
   numTestsKeptInMemory: 1,
   screenshotOnRunFailure: true,
-  trashAssetsBeforeRuns: false,
+  trashAssetsBeforeRuns: true,
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    overwrite: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  //  reporterOptions: {
+  //     reportDir: "cypress/results",
+  //     reportFilename:"report",
+  //     quite: true,
+  //     overwrite: true,
+  //     html: true,
+  //     json: true,
+  //     showPassed: false,
+  //     showFailed: false
+  //  },
   env: {
     staging: '/login',
     products_url: '/products',
   },
   e2e: {
+    // specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+      on('file:preprocessor', cucumber())
       // readFile
       on('task', {
         parseXlsx({ file, sheet }) {
@@ -33,3 +56,4 @@ module.exports = defineConfig({
   },
   // defaultCommandTimeout: 8000,
 });
+
